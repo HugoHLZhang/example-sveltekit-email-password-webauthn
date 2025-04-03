3d4d595996bfbdcbd24f958113bc89a4687a5fb1
Zhang Hugo <zhanghugo2711@outlook.com>
2025-04-03 11:25:36 +0200

    wip

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,18 +5,23 @@ import { get2FARedirect } from "$lib/server/2fa";
 import type { Actions, PageServerLoadEvent, RequestEvent } from "./$types";
 
 export function load(event: PageServerLoadEvent) {
+	// If the user is not authenticated, redirect to the login page
 	if (event.locals.session === null || event.locals.user === null) {
 		return redirect(302, "/login");
 	}
+	// If the user is not verified, redirect to the verify email page
 	if (!event.locals.user.emailVerified) {
 		return redirect(302, "/verify-email");
 	}
+	// If the user is not registered for 2FA, redirect to the 2FA setup page
 	if (!event.locals.user.registered2FA) {
 		return redirect(302, "/2fa/setup");
 	}
+	// If the user is not verified for 2FA, redirect to the 2FA verification page
 	if (!event.locals.session.twoFactorVerified) {
 		return redirect(302, get2FARedirect(event.locals.user));
 	}
+	// Return the user
 	return {
 		user: event.locals.user
 	};
