63c29d8def39a35949336c85d6440d5a90f72a94
Zhang Hugo <zhanghugo2711@outlook.com>
2025-04-03 14:28:38 +0200

    migrate to sv5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
+<script>
+	/** @type {{children?: import('svelte').Snippet}} */
+	let { children } = $props();
+</script>
+
 <svelte:head>
 	<title>Email and password example with 2FA and WebAuthn in SvelteKit</title>
 </svelte:head>
 
-<slot />
+{@render children?.()}
